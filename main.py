c93d6b24b87ba81a5eb5040160a6af5663dee73a
Eli Lutsky <elilutsky@Elis-MacBook-Pro.local>
2022-10-01 19:14:06 +0300

    Add all movies endpoint

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
+from typing import List
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
+movies: List[dict] = [
+    {
+        'name': 'Matrix',
+        'director': 'Steve',
+        'budget': 100000000,
+    },
+    {
+        'name': 'The Batman',
+        'director': 'Max',
+        'budget': 900000000,
+    },
+
+]
+
 
 @app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+async def root():
+    return 'Hello'
+
+
+@app.get("/movies")
+async def get_movies() -> List[dict]:
+    return movies
