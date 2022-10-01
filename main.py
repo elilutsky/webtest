463867989c1097b98a2061e4dcd5e8dace3a3d81
Eli Lutsky <elilutsky@Elis-MacBook-Pro.local>
2022-10-01 20:05:24 +0300

    Switch to BaseModel

--- main.py
+++ main.py
@@ -1,29 +1,26 @@
-from typing import List
-
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-movies: List[dict] = [
-    {
-        'name': 'Matrix',
-        'director': 'Steve',
-        'budget': 100000000,
-    },
-    {
-        'name': 'The Batman',
-        'director': 'Max',
-        'budget': 900000000,
-    },
 
+class Movie(BaseModel):
+    name: str
+    director: str
+    budget: int
+
+
+movies: list[Movie] = [
+    Movie(name='Matrix', director='Steve', budget=100000000),
+    Movie(name='The Batman', director='Max', budget=900000000)
 ]
 
 
-@app.get("/")
+@app.get('/')
 async def root():
     return 'Hello'
 
 
-@app.get("/movies")
-async def get_movies() -> List[dict]:
+@app.get('/movies')
+async def get_movies() -> list[Movie]:
     return movies
