52ca9c96b555cd666be441ecffde42b093591f1e
Eli Lutsky <elilutsky@Elis-MacBook-Pro.local>
2022-10-04 10:45:07 +0300

    Add get movie by name endpoint

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 
-from movies import Movie, get_all_movies
+from movies import Movie, get_all_movies, get_movie_by_name
 
 app = FastAPI()
 
@@ -10,6 +10,11 @@ async def root():
     return 'Hello'
 
 
-@app.get('/movies')
-async def get_movies() -> list[Movie]:
+@app.get('/movies', response_model=list[Movie])
+async def get_all_movies_api():
     return await get_all_movies()
+
+
+@app.get('/movies/{name}', response_model=Movie)
+async def get_movie_by_name_api(name: str):
+    return await get_movie_by_name(name)
