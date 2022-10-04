f0c82230927742b5d510b789965eb0504b915453
Eli Lutsky <elilutsky@Elis-MacBook-Pro.local>
2022-10-04 14:39:39 +0300

    Add type annotations to web server

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ async def root():
 
 
 @app.get('/movies', response_model=list[Movie])
-async def get_all_movies_api():
+async def get_all_movies_api() -> list[Movie]:
     return await get_all_movies()
 
 
 @app.get('/movies/{name}', response_model=Movie)
-async def get_movie_by_name_api(name: str):
+async def get_movie_by_name_api(name: str) -> Movie:
     movie = await get_movie_by_name(name)
     if movie is None:
         raise HTTPException(status_code=404, detail="Item not found")
