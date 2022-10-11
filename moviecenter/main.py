b4f9c87f8a756df93375d682622b0b68bd983ae3
Eli Lutsky <elilutsky@Elis-MacBook-Pro.local>
2022-10-11 13:51:26 +0300

    Use relative import

--- moviecenter/main.py
+++ moviecenter/main.py
@@ -2,8 +2,8 @@ import os
 
 from fastapi import FastAPI
 
-from moviecenter.app import movies_router
-from moviecenter.movies import MovieMongoClient
+from .app import movies_router
+from .movies import MovieMongoClient
 
 
 def get_mongodb_connection() -> MovieMongoClient:
